9326c4127b3101b4fcef586849661b0211a1d266
Spanhaak <mspanhaak@gmail.com>
2023-01-11 22:15:53 +0100

    Puzzle: detect pangram added

---
+++ detect-pangram/main.py
@@ -0,0 +1,25 @@
+import string
+def is_pangram(s):
+    ''' This function detects if the given input is a pangram '''
+    
+    # it is a bit lame, but I don't know how to do it without hardcoding the alphabet
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    
+    # remove punctuation and empty spaces
+    s = s.translate(str.maketrans('', '', string.punctuation))
+    s = s.replace(" ", "")
+    print(s)
+
+    # loop over the string and compare with the letters
+    for each_letter in letters:
+        if each_letter not in s.lower():
+            print("false")
+            return False
+
+    
+    print("true")
+    return True
+    
+
+
+is_pangram("The quick, brown fox jumps over the lazy dog!")
